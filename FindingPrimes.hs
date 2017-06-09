dfa968ce43249a2fdb2494dcd06b6e1a1bd843e7
Jung Jo Hyung <epikjjh@gamil.com>
2017-06-10 01:04:23 +0900

    Hw2.1 Done

--- FindingPrimes.hs
+++ FindingPrimes.hs
@@ -1,2 +1,15 @@
-Findingprimes Int -> Int -> [Int]
-Findingprimes n m
+isprime :: Int -> Bool
+isprime n = (length compare_list == 1)
+    where
+    -- Use definition of prime number
+    compare_list = [x|x <- [1..n-1], n `mod` x == 0] 
+
+-- Check if given number is positive
+ispositive :: Int -> Bool
+ispositive x = (x > 0)
+
+findingprimes :: Int -> Int -> [Int]
+findingprimes n m
+-- n could be negative number. However m must not be negative number
+    | m < 0 = error "Input argument2 must not be negative number"
+    | otherwise = take m (filter isprime (filter ispositive [n..]))
